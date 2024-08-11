fbbff2e9c70f85f1b3096709ef87983965c054b7
Aditi K <61196135+SavageSanta11@users.noreply.github.com>
2024-08-11 12:48:54 +0800

    fix hydration errors in sidebar toggling

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -5,8 +5,8 @@ import { useIsClient } from "usehooks-ts";
 import { cn } from "@/lib/utils";
 import { useSidebar } from "@/store/use-sidebar";
 
-import { Toggle } from "./toggle";
-
+import { ToggleSkeleton } from "./toggle";
+import { RecommendedSkeleton } from "./recommended";
 
 interface WrapperProps {
   children: React.ReactNode;
@@ -19,7 +19,8 @@ export const Wrapper = ({ children }: WrapperProps) => {
   if (!isClient) {
     return (
       <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
-        <Toggle />
+        <ToggleSkeleton />
+        <RecommendedSkeleton />
       </aside>
     );
   }
--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -3,7 +3,6 @@ import { getSelf } from "@/lib/auth-service";
 import { resolve } from "path";
 
 export const getRecommended = async () => {
-    await new Promise(resolve => setTimeout(resolve, 5000)); // makes it load for 5s
   const users = await db.user.findMany({
     orderBy: {
         createdAt: "desc"

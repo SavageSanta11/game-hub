5faa3598a8ff8f890d014b3782c8106441b6506f
Aditi K <61196135+SavageSanta11@users.noreply.github.com>
2024-08-12 22:47:10 +0800

    add follow service

---
+++ actions/follow.ts
@@ -0,0 +1,39 @@
+// makes sure this is protected and that the code doesn't fall into some javascript bundle
+
+"use server";
+
+import { revalidatePath } from "next/cache";
+
+import { followUser, unfollowUser } from "@/lib/follow-service";
+
+export const onFollow = async (id: string) => {
+  try {
+    const followedUser = await followUser(id);
+
+    revalidatePath("/");
+
+    if (followedUser) {
+      revalidatePath(`/${followedUser.following.username}`); // we can refresh server componenets so that they instantly have the newest data
+    }
+
+    return followedUser;
+  } catch (error) {
+    throw new Error("Internal Error");
+  }
+};
+
+export const onUnfollow = async (id: string) => {
+  try {
+    const unfollowedUser = await unfollowUser(id);
+
+    revalidatePath("/");
+
+    if (unfollowedUser) {
+      revalidatePath(`/${unfollowedUser.following.username}`); 
+    }
+
+    return unfollowedUser;
+  } catch (error) {
+    throw new Error("Internal Error");
+  }
+};
---
+++ app/(browse)/[username]/page.tsx
@@ -0,0 +1,28 @@
+import { isFollowingUser } from "@/lib/follow-service";
+import { getUserByUsername } from "@/lib/user-service";
+import { notFound } from "next/navigation";
+
+interface UserPageProps {
+  params: {
+    username: string;
+  };
+}
+
+const UserPage = async ({ params }: UserPageProps) => {
+  const user = await getUserByUsername(params.username)
+
+  if (!user){
+    notFound();
+  }
+
+  const isFollowing = await isFollowingUser(user.id);
+  return (
+    <div className="flex flex-col gap-y-4">
+        <p>User: {params.username}</p>
+        <p>User ID: {user.id}</p>
+        <p> Is following: {`${isFollowing}`}</p>
+    </div>
+  )
+};
+
+export default UserPage;
